98fad20c8fc32ff36f082524bdd8c4acfae3502f
Jessica Myer <36251741+oeddiemeddie@users.noreply.github.com>
2020-02-23 10:45:00 -0700

    here it is

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
+import { Link, Switch, Route } from 'react-router-dom'
+import History from '../History/History'
+import Contact from '../Contact/Contact'
 
 export default class About extends Component {
   render() {
     return (
       <div>
         <div className='subnav'>
+         <Link to='/about' className='subnav_links'>
+           <h3>About</h3>
+         </Link> 
+         <Link to='/about/history' className='subnav_links'>
+           <h3>History</h3>
+         </Link> 
+         <Link to='/about/contact' className='subnav_links'>
+           <h3>Contact</h3>
+         </Link>       
         </div>
         <div className='box'>
+          <Switch>
+            <Route path='/about/history' component={History}/>
+            <Route path='/about/contact' component={Contact}/>
+            <Route exact path='/about' render={ () => ( //renders JSX
+              <div>
+                <h1>About te University</h1>
+                <p>It's real good.</p>
+              </div>
+            )} />
+          </Switch>
         </div>
       </div>
     )
